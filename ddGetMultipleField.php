0dd27c382f08e27f78cb48e7ad9422591f45c127
Ilyas Ronef <ilyas@divandesign.biz>
2016-12-08 18:56:09 +0500

    * Minor changes.

--- ddGetMultipleField.php
+++ ddGetMultipleField.php
@@ -1,44 +1,43 @@
 <?php
 /**
- * ddGetMultipleField.php
+ * ddGetMultipleField
  * @version 3.3 (2016-06-06)
  * 
  * @desc A snippet for separated by delimiters data output.
  * @note The fields formed by the mm_ddMultipleFields widget values ooutput gets more convinient with the snippet.
  * 
  * @uses MODX >= 1.0.13.
- * @uses The library modx.ddTools 0.15.3.
- * @uses The snippet ddTypograph 1.4.3 (if typography is required).
+ * @uses MODXEvo.library.ddTools >= 0.15.3.
+ * @uses MODXEvo.snippet.ddTypograph >= 1.4.3 (if typography is required).
  * 
- * @param $inputString {separated string} - The input string containing separated values. @required
- * @param $inputString_docField {string} - The name of the document field/TV which value is required to get. If the parameter is passed then the input string will be taken from the field/TV and “inputString” will be ignored. Default: —.
- * @param $inputString_docId {integer} - ID of the document which field/TV value is required to get. “inputString_docId” equals the current document id since “inputString_docId” is unset. Default: —.
- * @param $rowDelimiter {string; regexp} - The input string row delimiter. Default: '||'.
- * @param $colDelimiter {string; regexp} - The input string column delimiter. Default: '::'.
- * @param $startRow {integer} - The index of the initial row (indexes start at 0). Default: 0.
- * @param $totalRows {integer; 'all'} - The maximum number of rows to return. All rows will be returned if “totalRows” == 'all'. Default: 'all'.
- * @param $columns {comma separated string; 'all'} - The indexes of columns to return (indexes start at 0). All columns will be returned if “columns” == 'all'. Default: 'all'.
- * @param $filter {separated string} - Filter clause for columns. Thus, '0::a||0::b||1::1' makes the columns with either 'a' or 'b' in the 0 column and with 1 in the 1 column to be returned. Default: ''.
- * @param $removeEmptyRows {0; 1} - Is it required to remove empty rows? Default: 1.
- * @param $removeEmptyCols {0; 1} - Is it required to remove empty columns? Default: 1.
- * @param $sortBy {comma separated string} - The index of the column to sort by (indexes start at 0). The parameter also takes comma-separated values for multiple sort, e.g. '0,1'. Default: '0'.
- * @param $sortDir {'ASC'; 'DESC'; 'RAND'; 'REVERSE'; ''} - Rows sorting direction. The rows will be returned in reversed order if “sortDir” == 'REVERSE'. Default: ''.
- * @param $typography {comma separated string} - The comma separated indexes of the columns which values have to be corrected (indexes start at 0). If unset, there will be no correction. Default: —.
- * @param $outputFormat {'html'; 'JSON'; 'array'; 'htmlarray'} - Result output format. Default: 'html'.
- * @param $rowGlue {string} - The string that combines rows while rendering. It can be used along with “rowTpl”. Default: ''.
- * @param $colGlue {string} - The string that combines columns while rendering. It can be used along with “colTpl”, but not with “rowTpl” for obvious reasons. Default: ''.
- * @param $rowTpl {string: chunkName|string} - The template for row rendering (“outputFormat” has to be == 'html'). Use inline templates starting with “@CODE:”. Available placeholders: [+rowNumber+] (index of current row, starts at 1), [+rowNumber.zeroBased+] (index of current row, starts at 0), [+total+] (total number of rows), [+resultTotal+] (total number of returned rows), [+col0+],[+col1+],… (column values). Default: ''.
- * @param $colTpl {comma separated string: chunkName|string; 'null'} - The comma-separated list of templates for column rendering (“outputFormat” has to be == 'html'). Use inline templates starting with “@CODE:”. If the number of templates is lesser than the number of columns then the last passed template will be used to render the rest of the columns. 'null' specifies rendering without a template. Available placeholders: [+val+], [+rowNumber+] (index of current row, starts at 1), [+rowNumber.zeroBased+] (index of current row, starts at 0). Default: ''.
- * @param $outerTpl {string: chunkName|string} - Wrapper template (“outputFormat” has to be != 'array'). Use inline templates starting with “@CODE:”. Available placeholders: [+result+], [+total+] (total number of rows), [+resultTotal+] (total number of returned rows), [+rowY.colX+] (“Y” — row number, “X” — column number). Default: ''.
- * @param $placeholders {query string} - Additional data as query string (https://en.wikipedia.org/wiki/Query_string) has to be passed into “outerTpl”, “rowTpl” and “colTpl”. E. g. “pladeholder1=value1&pagetitle=My awesome pagetitle!”. Arrays are supported too: “some[a]=one&some[b]=two” => “[+some.a+]”, “[+some.b+]”; “some[]=one&some[]=two” => “[+some.0+]”, “[some.1]”. Default: ''.
- * @param $urlencode {0; 1} - Is it required to URL encode the result? “outputFormat” has to be != 'array'. URL encoding is used according to RFC 3986. Default: 0.
- * @param $totalRowsToPlaceholder {string} - The name of the global MODX placeholder that holds the total number of rows. The placeholder won't be set if “totalRowsToPlaceholder” is empty. Default: ''.
- * @param $resultToPlaceholder {string} - The name of the global MODX placeholder that holds the snippet result. The result will be returned in a regular manner if the parameter is empty. Default: ''.
+ * @param $inputString {string_separated} — The input string containing separated values. @required
+ * @param $inputString_docField {string} — The name of the document field/TV which value is required to get. If the parameter is passed then the input string will be taken from the field/TV and “inputString” will be ignored. Default: —.
+ * @param $inputString_docId {integer} — ID of the document which field/TV value is required to get. “inputString_docId” equals the current document id since “inputString_docId” is unset. Default: —.
+ * @param $rowDelimiter {string|regexp} — The input string row delimiter. Default: '||'.
+ * @param $colDelimiter {string|regexp} — The input string column delimiter. Default: '::'.
+ * @param $startRow {integer} — The index of the initial row (indexes start at 0). Default: 0.
+ * @param $totalRows {integer|'all'} — The maximum number of rows to return. All rows will be returned if “totalRows” == 'all'. Default: 'all'.
+ * @param $columns {string_commaSeparated|'all'} — The indexes of columns to return (indexes start at 0). All columns will be returned if “columns” == 'all'. Default: 'all'.
+ * @param $filter {string_separated} — Filter clause for columns. Thus, '0::a||0::b||1::1' makes the columns with either 'a' or 'b' in the 0 column and with 1 in the 1 column to be returned. Default: ''.
+ * @param $removeEmptyRows {0|1} — Is it required to remove empty rows? Default: 1.
+ * @param $removeEmptyCols {0|1} — Is it required to remove empty columns? Default: 1.
+ * @param $sortBy {string_commaSeparated} — The index of the column to sort by (indexes start at 0). The parameter also takes comma-separated values for multiple sort, e.g. '0,1'. Default: '0'.
+ * @param $sortDir {'ASC'|'DESC'|'RAND'|'REVERSE'|''} — Rows sorting direction. The rows will be returned in reversed order if “sortDir” == 'REVERSE'. Default: ''.
+ * @param $typography {string_commaSeparated} — The comma separated indexes of the columns which values have to be corrected (indexes start at 0). If unset, there will be no correction. Default: —.
+ * @param $outputFormat {'html'|'JSON'|'array'|'htmlarray'} — Result output format. Default: 'html'.
+ * @param $rowGlue {string} — The string that combines rows while rendering. It can be used along with “rowTpl”. Default: ''.
+ * @param $colGlue {string} — The string that combines columns while rendering. It can be used along with “colTpl”, but not with “rowTpl” for obvious reasons. Default: ''.
+ * @param $rowTpl {string_chunkName|string} — The template for row rendering (“outputFormat” has to be == 'html'). Use inline templates starting with “@CODE:”. Available placeholders: [+rowNumber+] (index of current row, starts at 1), [+rowNumber.zeroBased+] (index of current row, starts at 0), [+total+] (total number of rows), [+resultTotal+] (total number of returned rows), [+col0+],[+col1+],… (column values). Default: ''.
+ * @param $colTpl {string_commaSeparated_chunkName|string|'null'} — The comma-separated list of templates for column rendering (“outputFormat” has to be == 'html'). Use inline templates starting with “@CODE:”. If the number of templates is lesser than the number of columns then the last passed template will be used to render the rest of the columns. 'null' specifies rendering without a template. Available placeholders: [+val+], [+rowNumber+] (index of current row, starts at 1), [+rowNumber.zeroBased+] (index of current row, starts at 0). Default: ''.
+ * @param $outerTpl {string_chunkName|string} — Wrapper template (“outputFormat” has to be != 'array'). Use inline templates starting with “@CODE:”. Available placeholders: [+result+], [+total+] (total number of rows), [+resultTotal+] (total number of returned rows), [+rowY.colX+] (“Y” — row number, “X” — column number). Default: ''.
+ * @param $placeholders {string_queryFormated} — Additional data as query string (https://en.wikipedia.org/wiki/Query_string) has to be passed into “outerTpl”, “rowTpl” and “colTpl”. E. g. “pladeholder1=value1&pagetitle=My awesome pagetitle!”. Arrays are supported too: “some[a]=one&some[b]=two” => “[+some.a+]”, “[+some.b+]”; “some[]=one&some[]=two” => “[+some.0+]”, “[some.1]”. Default: ''.
+ * @param $urlencode {0|1} — Is it required to URL encode the result? “outputFormat” has to be != 'array'. URL encoding is used according to RFC 3986. Default: 0.
+ * @param $totalRowsToPlaceholder {string} — The name of the global MODX placeholder that holds the total number of rows. The placeholder won't be set if “totalRowsToPlaceholder” is empty. Default: ''.
+ * @param $resultToPlaceholder {string} — The name of the global MODX placeholder that holds the snippet result. The result will be returned in a regular manner if the parameter is empty. Default: ''.
  * 
  * @link http://code.divandesign.biz/modx/ddgetmultiplefield/3.3
  * 
  * @copyright 2009–2016 DivanDesign {@link http://www.DivanDesign.biz }
- * http://www.DivanDesign.biz
  */
 
 $ddToolsPath = $modx->getConfig('base_path').'assets/libs/ddTools/modx.ddtools.class.php';
@@ -137,14 +136,22 @@ if (isset($inputString) && strlen($inputString) > 0){
 			}
 		}
 		
-		//Если нужно получить какую-то конкретную колонку (также проверяем на то, что строка вообще существует, т.к. она могла быть уже удалена ранее)
-		if ($columns != 'all' && isset($data[$rowNumber])){
+		//Если нужно получить какую-то конкретную колонку
+		if (
+			$columns != 'all' &&
+			//Также проверяем на то, что строка вообще существует, т.к. она могла быть уже удалена ранее
+			isset($data[$rowNumber])
+		){
 			//Выбираем только необходимые колонки + Сбрасываем ключи массива
 			$data[$rowNumber] = array_values(array_intersect_key($data[$rowNumber], $columns));
 		}
 		
-		//Если нужно удалять пустые строки (также проверяем на то, что строка вообще существует, т.к. она могла быть уже удалена ранее)
-		if ($removeEmptyRows && isset($data[$rowNumber])){
+		//Если нужно удалять пустые строки
+		if (
+			$removeEmptyRows &&
+			//Также проверяем на то, что строка вообще существует, т.к. она могла быть уже удалена ранее
+			isset($data[$rowNumber])
+		){
 			//Если строка пустая, удаляем
 			if (strlen(implode('', $data[$rowNumber])) == 0){unset($data[$rowNumber]);}
 		}
@@ -173,13 +180,23 @@ if (isset($inputString) && strlen($inputString) > 0){
 			}
 		}
 		
-		if (!isset($startRow) || !is_numeric($startRow)){$startRow = '0';}
+		if (
+			!isset($startRow) ||
+			!is_numeric($startRow)
+		){
+			$startRow = '0';
+		}
 		
 		//Обрабатываем слишком большой индекс
 		if (!isset($data[$startRow])){$startRow = count($data) - 1;}
 		
 		//Если общее количество элементов не задано или задано плохо, читаем, что нужны все
-		if (!isset($totalRows) || !is_numeric($totalRows)){$totalRows = 'all';}
+		if (
+			!isset($totalRows) ||
+			!is_numeric($totalRows)
+		){
+			$totalRows = 'all';
+		}
 		
 		//Если нужны все элементы
 		if ($totalRows == 'all'){
@@ -219,7 +236,10 @@ if (isset($inputString) && strlen($inputString) > 0){
 			$resTemp = array();
 			
 			//Дополнительные данные
-			if (isset($placeholders) && trim($placeholders) != ''){
+			if (
+				isset($placeholders) &&
+				trim($placeholders) != ''
+			){
 				//If “=” exists
 				if (strpos($placeholders, '=') !== false){
 					//Parse a query string
@@ -236,7 +256,10 @@ if (isset($inputString) && strlen($inputString) > 0){
 			}
 			
 			//Если вывод просто в формате html
-			if ($outputFormat == 'html' || $outputFormat == 'htmlarray'){
+			if (
+				$outputFormat == 'html' ||
+				$outputFormat == 'htmlarray'
+			){
 				//Шаблоны колонок
 				$colTpl = isset($colTpl) ? explode(',', $colTpl) : false;
 				
@@ -275,11 +298,17 @@ if (isset($inputString) && strlen($inputString) > 0){
 						//Перебираем колонки
 						foreach ($row as $columnNumber => $column){
 							//Если нужно удалять пустые значения
-							if ($removeEmptyCols && !strlen($column)){
+							if (
+								$removeEmptyCols &&
+								!strlen($column)
+							){
 								$resTemp[$rowNumber]['col'.$columnNumber] = '';
 							}else{
 								//Если есть шаблоны значений колонок
-								if ($colTpl !== false && strlen($colTpl[$columnNumber]) > 0){
+								if (
+									$colTpl !== false &&
+									strlen($colTpl[$columnNumber]) > 0
+								){
 									$resTemp[$rowNumber]['col'.$columnNumber] = $modx->parseText($colTpl[$columnNumber], array_merge(array(
 										'val' => $column,
 										'rowNumber.zeroBased' => $resTemp[$rowNumber]['rowNumber.zeroBased'],
@@ -298,7 +327,10 @@ if (isset($inputString) && strlen($inputString) > 0){
 						//Если есть шаблоны значений колонок
 						if ($colTpl !== false){
 							foreach ($row as $columnNumber => $column){
-								if ($removeEmptyCols && !strlen($column)){
+								if (
+									$removeEmptyCols &&
+									!strlen($column)
+								){
 									unset($row[$columnNumber]);
 								}else if (strlen($colTpl[$columnNumber]) > 0){
 									$row[$columnNumber] = $modx->parseText($colTpl[$columnNumber], array_merge(array(
@@ -323,7 +355,10 @@ if (isset($inputString) && strlen($inputString) > 0){
 				$resTemp = $data;
 				
 				//Если нужно выводить только одну колонку
-				if ($columns != 'all' && count($columns) == 1){
+				if (
+					$columns != 'all' &&
+					count($columns) == 1
+				){
 					$resTemp = array_map('implode', $resTemp);
 				}
 				
